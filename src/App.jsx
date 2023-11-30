a03535cdc39284d13a09c3f3e229c4e0a1c25c78
Sebastian Sarango <jairsebastiansarango@gmail.com>
2023-11-30 17:29:59 -0300

    [feat]- cambios menores en APP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import HomeView from './core/layouts/views/HomeView'
 import { register as registerSwiper } from 'swiper/element/bundle';
-import LoginView from './core/layouts/views/LoginView';
 import RootProvider from './core/providers/RootProvider';
 import { RouterProvider } from 'react-router-dom';
 import { AppRouter } from './core/routes/AppRouter';
