d9d7e005ace946e2b68fa2cc8f0cd6d765e71447
Sebastian Sarango <jairsebastiansarango@gmail.com>
2023-12-04 21:09:07 -0300

    [feat - nav-dropdon-finish]

--- src/core/components/nav/Nav.jsx
+++ src/core/components/nav/Nav.jsx
@@ -1,34 +1,60 @@
-import React from 'react'
-import logo from '../../../assets/logo.jpeg'
-import '../../styles/nav.css'
-import { useAuth } from '../../auth/hook/useAuth'
+import React from "react";
+import logo from "../../../assets/logo.jpeg";
+import "../../styles/nav.css";
+import { useAuth } from "../../auth/hook/useAuth";
+import { Icon } from "@iconify/react";
 
 const Nav = () => {
-
-  const{logout, toggleConfig, showConfig} = useAuth();
+  const { logout, toggleConfig, showConfig } = useAuth();
   return (
-    <div className='nav'>
-        <img className='nav-logo' src={logo} alt="logo de cineverse"/>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Movies</a></li>
-          <li><a href="#">Series</a></li>
-        </ul>
-        <div className='nav-item' onClick={toggleConfig}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="usuario 1" className='nav-user' />
-          <p>User 1</p>         
-        </div> 
-        <div className={showConfig?'nav-dropdown-active':'nav-dropdown'}>
-            <ul className='nav-dropdown-list'>
-              <li>Cosa 1</li>
-              <li>Cosa 2</li>
-              <li>Cosa 3</li>
-              <li onClick={logout}>Cerrar sesion</li>
-            </ul>
+    <div className="nav">
+      <img className="nav-logo" src={logo} alt="logo de cineverse" />
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Movies</a>
+        </li>
+        <li>
+          <a href="#">Series</a>
+        </li>
+      </ul>
+      <div
+        className={showConfig ? "nav-item-active" : "nav-item"}
+        onClick={toggleConfig}
+      >
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt="usuario 1"
+          className="nav-user"
+        />
+        <p>User 1</p>
+      </div>
+      <div className={showConfig ? "nav-dropdown-active" : "nav-dropdown"}>
+        <ul className="nav-dropdown-list">
+          <div className="nav-dropdown-content">
+            <Icon icon="mi:edit" color="white" />
+            <li>Manage Profile</li>
           </div>
-
+          <div className="nav-dropdown-content">
+            <Icon icon="line-md:account" color="white" />
+            <li className="nav-dropdown-account">Account</li>
+          </div>
+          <div className="nav-dropdown-content">
+            <Icon icon="material-symbols:help-outline" color="white" />
+            <li className="nav-dropdown-help">Help Centre</li>
+          </div>
+          <div className="nav-dropdown-content logout">
+          <Icon icon="line-md:logout" color="white" />
+            <li className="nav-dropdown-logout" onClick={logout}>
+              Cerrar sesion
+            </li>
+          </div>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
